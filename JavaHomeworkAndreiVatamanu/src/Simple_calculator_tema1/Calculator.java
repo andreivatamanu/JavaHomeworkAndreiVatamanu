eba2ee7ed4cedea44dcb95fb49237a4c414f7cae
andreivatamanu <andrei.vatamanu.01@gmail.com>
2017-06-20 17:59:59 +0300

    Update Calculator.java

--- JavaHomeworkAndreiVatamanu/src/Simple_calculator_tema1/Calculator.java
+++ JavaHomeworkAndreiVatamanu/src/Simple_calculator_tema1/Calculator.java
@@ -8,12 +8,16 @@ package Simple_calculator_tema1;
 import java.util.*;
 public class Calculator {
     public static void main(String[] args) {
+        
+       // Declaring variables
          int x;
          int y;
-
+// Pregatim citirea datelor de la tastatura
          Scanner input=new Scanner(System.in);
+         //requesting first number
          System.out.println("Please introduce your first number:");
          x=input.nextInt();
+        //requesting second number
          System.out.println("Please introduce second number");
          y=input.nextInt();
 
