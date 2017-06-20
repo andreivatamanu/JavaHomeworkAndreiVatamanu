8ecae518960cd9f996db54f857f364a2efdbe39a
andreivatamanu <andrei.vatamanu.01@gmail.com>
2017-06-20 18:03:20 +0300

    Update JavaHomeworkAndreiVatamanu.java

--- JavaHomeworkAndreiVatamanu/src/javahomeworkandreivatamanu/JavaHomeworkAndreiVatamanu.java
+++ JavaHomeworkAndreiVatamanu/src/javahomeworkandreivatamanu/JavaHomeworkAndreiVatamanu.java
@@ -4,39 +4,5 @@
  */
 package javahomeworkandreivatamanu;
 
-import java.util.*;
-public class JavaHomeworkAndreiVatamanu {
-    
-    public static void main(String[] args) {
-        
-         // Declaring variables
-         int x;
-         int y;
- // Pregatim citirea datelor de la tastatura
-         Scanner input=new Scanner(System.in);
-         //requesting first number
-         System.out.println("Please introduce your first number:");
-         x=input.nextInt();
-         //requesting second number
-         System.out.println("Please introduce second number");
-         y=input.nextInt();
 
-         System.out.println("Please enter operation + - / or *");
-         Scanner op=new Scanner(System.in);
-         String operation = op.next();
-
-         if (operation.equals("+")){
-             System.out.println("Result is:  " + (x+y));
-         }
-         if (operation.equals("-")){
-             System.out.println("Result is:  "+ (x-y));
-         }
-         if (operation.equals("/")){
-             System.out.println("Result is:  "+ (x/y));
-         }
-         if (operation.equals("*")){
-            System.out.println("Result is:  "+ (x*y));
-    }
-    }
-}
 
